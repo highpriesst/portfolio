d62a50adaceaed8b6c1f72d1dd3bc37f8a65ae7b
highpriesst <ertansofia@gmail.com>
2022-10-14 18:31:31 +0300

    trying out design for mobile first

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 
-const dark = false;
+import Image from "next/image";
 
 //Todo: Use link components from nextjs instead the li component down below.
 
@@ -13,22 +12,33 @@ const dark = false;
 
 //Todo: Check the devslife page for inspiration
 
-//Design this for the MOBILE first !
-
 //Make sure navbar is complete before moving to footer.
+
+//DO only dark theme
 
 export const Navbar = () => {
   return (
-    <div className="flex list-none gap-5 justify-center text-white items-center pt-2 font-thin">
-      <li className=" font-normal">Ertan Mutlu</li>
-      <li>Works</li>
-      <li>Posts</li>
-      <li>About</li>
-      {dark ? (
-        <MoonIcon className="text-white h-4 w-4 hover:text-orange-200" />
-      ) : (
-        <SunIcon className="text-white h-6 w-6 hover:text-blue-200" />
-      )}
+    <div className="flex list-none gap-5 justify-between sm:justify-center text-white items-center p-3 md:pt-2 font-thin">
+      <li className=" font-normal">
+        <span className="pl-2">Ertan Mutlu</span>
+      </li>
+      <li className=" hidden sm:block">Works</li>
+      <li className=" hidden sm:block">Posts</li>
+      <li className=" hidden sm:block">About</li>
+
+      <svg
+        className="w-6 h-6 sm:hidden"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg">
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M4 6h16M4 12h16M4 18h7"
+        />
+      </svg>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,19 @@ import Head from "next/head";
 import Image from "next/image";
 import { Navbar } from "../components/navbar";
 
+// Ana sayfaya girerken bi logo animasyonu yap? Icinde 3 yazsin.
+
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Ertan Mutlu</title>
       </Head>
-      <div className="">
-        <div className="bg-gray-700">
-          <Navbar />
+
+      {/* TODO: Anasayfa animasyonu */}
+      <div className="bg-gray-700">
+        <Navbar />
+        <div className="flex justify-center items-center h-screen bg-gray-700">
           <h1 className="font-bold">Hello world</h1>
         </div>
       </div>
