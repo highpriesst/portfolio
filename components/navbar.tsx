9aa1d70fd88173f10bde1563e9af773616f092d6
highpriesst <ertansofia@gmail.com>
2022-10-27 20:37:55 +0300

    trying out some animations

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,6 +6,8 @@ import {
   PaperAirplaneIcon,
 } from "@heroicons/react/24/outline";
 
+import { motion } from "framer-motion";
+
 //Todo: Use link components from nextjs instead the li component down below.
 
 //Make sure all the links are working fine.
@@ -25,6 +27,10 @@ export const Navbar = () => {
     setActive(!active);
   };
 
+  const BoxStyled = styled(motion.div)`
+    display: flex;
+  `;
+
   return (
     <>
       <nav
@@ -37,23 +43,30 @@ export const Navbar = () => {
             Ertan
           </a>
         </Link>
-        <button
-          className=" inline-flex p-3 text-black dark:hover:bg-gray-800 rounded lg:hidden dark:text-white ml-auto hover:bg-gray-100 outline-none"
-          onClick={handleClick}>
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        </button>
+        <motion.div
+          transition={{
+            delay: 0.5,
+            x: { duration: 1 },
+            default: { easee: "linear" },
+          }}>
+          <button
+            className=" inline-flex p-3 text-black dark:hover:bg-gray-800 rounded lg:hidden dark:text-white ml-auto hover:bg-gray-100 outline-none"
+            onClick={handleClick}>
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </motion.div>
         {/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
         <div
           className={`${
