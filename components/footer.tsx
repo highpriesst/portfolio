3cd23f327210d77b4a4b4656492217b8d9d2ffb1
highpriesst <ertansofia@gmail.com>
2022-10-29 20:52:55 +0300

    changed it to darker background

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="flex justify-center items-center text-gray-400 bg-gray-100 dark:bg-gray-800 dark:text-gray-600">
+    <div className="flex justify-center items-center text-gray-400 bg-gray-100 dark:bg-gray-900 dark:text-gray-600 p-3">
       ©2022 Ertan Mutlu. All Rights Reserved.
     </div>
   );
