b8f323d4250696e1598874195eae853c5ead67ee
highpriesst <ertansofia@gmail.com>
2022-10-31 20:55:43 +0200

    making sure framer works adding motion. to the front

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Link from "next/link";
 import { useState } from "react";
 import { Home, Projects, About, Contact } from "../components";
+import { motion } from "framer-motion";
 
 // import { motion, useScroll } from "framer-motion";
 
@@ -14,26 +15,26 @@ export default function Main() {
       </Head>
 
       <div className="snap-y snap-mandatory overflow-scroll h-screen w-screen">
-        <section
+        <motion.section
           id="home"
           className="flex justify-center items-center h-screen w-screen bg-gray-100 dark:bg-gray-900 text-white snap-start">
           <Home />
-        </section>
-        <section
+        </motion.section>
+        <motion.section
           id="projects"
           className="flex justify-center items-center h-screen w-screen bg-gray-100 dark:bg-gray-800 text-white  snap-start">
           <Projects />
-        </section>
-        <section
+        </motion.section>
+        <motion.section
           id="about"
           className="flex justify-center items-center h-screen w-screen bg-gray-100 dark:bg-gray-700 text-white  snap-start">
           <About />
-        </section>
-        <section
+        </motion.section>
+        <motion.section
           id="contact"
           className="flex justify-center items-center h-screen w-screen bg-gray-100 dark:bg-gray-600 text-white snap-start">
           <Contact />
-        </section>
+        </motion.section>
       </div>
     </div>
   );
