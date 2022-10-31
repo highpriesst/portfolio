d07b47e0da09faadbfc2cb04a1d15d8b9b6c2b95
highpriesst <ertansofia@gmail.com>
2022-10-31 20:54:34 +0200

    adding framer animation when in view

--- components/sections/contactSection.tsx
+++ components/sections/contactSection.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 function Contact() {
   return (
-    <section className="flex justify-center items-center h-screen w-screen bg-gray-100 dark:bg-gray-600 text-white  snap-start">
+    <motion.section
+      initial={{ x: "-100vw" }}
+      animate={{ x: 0 }}
+      transition={{ type: "spring", duration: 1, bounce: 0.3 }}
+      className="flex justify-center items-center h-screen w-screen bg-gray-100 dark:bg-gray-600 text-white  snap-start">
       <h1>contact</h1>
-    </section>
+    </motion.section>
   );
 }
 
--- components/sections/homeSection.tsx
+++ components/sections/homeSection.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 
+import { motion } from "framer-motion";
+
 function homeSection() {
   return (
-    <section className="flex justify-center items-center h-screen w-screen bg-gray-100 dark:bg-gray-900 text-white  snap-start">
+    <motion.section
+      className="flex justify-center items-center h-screen w-screen bg-gray-100 dark:bg-gray-900 text-white  snap-start"
+      initial={{ x: "-100vw" }}
+      animate={{ x: 0 }}
+      transition={{ type: "spring", duration: 1, bounce: 0.3 }}>
       <h1>home Page</h1>
-    </section>
+    </motion.section>
   );
 }
 
