e90a9079881e1c168eba4c947481277f8c8612bf
highpriesst <ertansofia@gmail.com>
2022-11-07 15:26:11 +0300

    added individual component and experiment its types

--- components/project-component/index.tsx
+++ components/project-component/index.tsx
@@ -1,27 +1,37 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import Cards, { CardProps } from "../../types/componentType";
 
 // { image, title, description }
 
-const PComponent: React.FC<CardProps> = ({ cards }: CardProps) => {
+const PComponent: React.FC<Cards> = ({
+  id,
+  image,
+  title,
+  description,
+}: Cards) => {
   return (
-    <div className="relative flex min-h-screen flex-col justify-center bg-gradient-to-r from-rose-100 to-teal-100">
-      <div className="mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
+    <div className="relative flex min-h-screen flex-col justify-center" id={id}>
+      <div className=" flex w-auto flex-col justify-center item-center cursor-pointer rounded-2xl border border-paleGray-100">
         <Image
-          className="aspect-video w-96 rounded-t-2xl object-cover object-center"
-          src="https://images.pexels.com/photos/3311574/pexels-photo-3311574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          className="rounded-t-2xl object-cover object-center"
+          src={image}
           alt="thumbnail"
+          width={100}
+          height={120}
         />
-
         <div className="p-4">
-          <h1 className="text-2xl font-medium text-slate-600 pb-2">
-            {cards.title}
+          <h1 className="text-2xl font-medium pb-2 text-shinyRed-500">
+            {title}
           </h1>
-          <p className="text-sm tracking-tight font-light text-slate-400 leading-6">
-            {cards.description}
+          <p className="text-sm tracking-tight font-light text-paleGray-100 leading-6">
+            {description}
           </p>
         </div>
+        <Link className=" text-paleGray-800" href="/about">
+          Go to projects {"=>"}
+        </Link>
       </div>
     </div>
   );
