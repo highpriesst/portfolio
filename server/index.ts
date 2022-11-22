9e4b3edb51e8519b9cb6f23093a71dfe2e50f768
highpriesst <ertansofia@gmail.com>
2022-11-22 18:28:19 +0200

    setting up server with socket.io

--- server/index.ts
+++ server/index.ts
@@ -1,2 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+
+const port = 3001;
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket: any) => {
+  console.log(socket.id);
+
+  io.on("disconnect", () => {
+    console.log("User disconnected, ", socket.id);
+  });
+});
+
+server.listen(port, () => {
+  console.log("Server runnning on port: " + port);
+});
