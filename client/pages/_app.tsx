a59f38b9415efb8a2e1449ab20ff3bf394020bc9
highpriesst <ertansofia@gmail.com>
2022-11-28 22:58:50 +0200

    components folder, socketContext

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import SocketsProvider from "../context/socketContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    //Enables that you can use socket from anywhere in the app.
+    <SocketsProvider>
+      <Component {...pageProps} />
+    </SocketsProvider>
+  );
 }
