20a8321fefb1bcb80e602d664f490b3bc6b60f18
highpriesst <ertansofia@gmail.com>
2022-12-08 13:43:08 +0300

    structure files moving

--- components/ThemeSwither.js
+++ components/ThemeSwither.js
@@ -1,46 +1,37 @@
+import { motion as m } from "framer-motion";
 import { useState } from "react";
+import { RiMoonClearFill, RiSunFill } from "react-icons/ri";
 
 function ThemeSwither() {
   const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => setDarkMode(!darkMode);
+
+  const spring = {
+    type: "spring",
+    stiffness: 700,
+    damping: 30,
+  };
+
   return (
-    <div className="flex gap-2">
-      {/* Sends request to my gmail */}
-      <button className="m-2">Hire Me</button>
-      {/* React icons or hero icons */}
-      <button
-        onClick={() => setDarkMode(!darkMode)}
-        className=" appearance-none">
-        {darkMode ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-            />
-          </svg>
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
-            />
-          </svg>
-        )}
-      </button>
-    </div>
+    <m.div
+      onClick={toggleDarkMode}
+      className={`flex-start flex h-[50px] w-[100px] rounded-[50px] bg-zinc-100 p-[5px] shadow-inner hover:cursor-pointer dark:bg-zinc-700 ${
+        darkMode && "place-content-end"
+      }`}>
+      <m.div
+        className="flex h-[40px] w-[40px] items-center justify-center rounded-full bg-black/90"
+        layout
+        transition={spring}>
+        <m.div whileTap={{ rotate: 360 }}>
+          {darkMode ? (
+            <RiSunFill className="h-6 w-6 text-yellow-300" />
+          ) : (
+            <RiMoonClearFill className="h-6 w-6 text-slate-200" />
+          )}
+        </m.div>
+      </m.div>
+    </m.div>
   );
 }
 
