76db8bc91c9d8ffb3ba393e6ca44de8ed675f730
highpriesst <ertansofia@gmail.com>
2022-12-23 22:41:08 +0200

    new footer

---
+++ sections/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-white dark:bg-gray-900 text-center lg:text-left">
+      <div className=" text-center p-4 bg-black-opacity-20">
+        © 2021 Copyright &#160;
+        <a className="text-color-dark" href="https://github.com/highpriesst">
+          Highpriesst
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
