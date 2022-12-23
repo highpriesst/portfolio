1f63d2414998fa16d51cb86cbb30dce7145421d0
highpriesst <ertansofia@gmail.com>
2022-12-23 22:41:00 +0200

    added the newly created footer to everypage to show

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import "../styles/globals.css";
 import Navbar from "../sections/Navbar";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import Footer from "../sections/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
       <Navbar />
       <Component {...pageProps} />
+      <Footer />
     </ThemeProvider>
   );
 }
