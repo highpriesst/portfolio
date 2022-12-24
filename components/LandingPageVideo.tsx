6c31d460b718b42f768e6cc34150d3d97a7511d6
highpriesst <ertansofia@gmail.com>
2022-12-24 12:54:24 +0200

    video background fixed

--- components/LandingPageVideo.tsx
+++ components/LandingPageVideo.tsx
@@ -2,15 +2,14 @@ import React from "react";
 
 function LandingPageVideo() {
   return (
-    <div className="h-screen">
-      <header className="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
-        <div className="relative z-30 p-5 text-2xl text-white bg-purple-300 bg-opacity-50 rounded-xl">
-          Welcome to my site!
-        </div>
-        <video autoPlay loop muted className="w-full h-screen z-10">
-          <source src="/background-video" type="video/mp4" />
-        </video>
-      </header>
+    <div className="bg-hero-section bg-no-repeat h-screen bg-center bg-cover">
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute inset-0 object-cover h-screen xl:h-auto">
+        <source src="assests/background-video.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
