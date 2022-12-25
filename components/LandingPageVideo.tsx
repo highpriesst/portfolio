d9bf9b3596434dcbb7e8218fd116bc232042cc61
highpriesst <ertansofia@gmail.com>
2022-12-25 20:04:55 +0200

    changing the video

--- components/LandingPageVideo.tsx
+++ components/LandingPageVideo.tsx
@@ -4,7 +4,7 @@ import { motion as m } from "framer-motion";
 
 const LandingPageWithVideo = () => {
   return (
-    <div className="bg-gray-900 min-h-screen">
+    <m.div className="bg-gray-900 min-h-screen">
       <Head>
         <title>Ertan</title>
       </Head>
@@ -15,17 +15,17 @@ const LandingPageWithVideo = () => {
           loop
           muted
           className="absolute inset-0 object-cover h-screen xl:h-auto">
-          <source src="assests/background-video.mp4" type="video/mp4" />
+          <source src="assests/cyberpunk-video.mp4" type="video/mp4" />
         </video>
       </div>
 
       {/* button scroll down icon and transparent blur animation is looped up and down */}
-      <div className="relative z-9 flex flex-col items-center justify-center h-screen px-4 py-12 text-center text-white">
-        <h1 className="text-3xl">Welcome!</h1>
+      <m.div className="relative z-9 flex flex-col items-center justify-center h-screen px-4 py-12 text-center text-white">
+        <m.h1 className="text-3xl">Welcome!</m.h1>
         <button className="px-6 py-3 rounded-full bg-white text-gray-900 font-bold hover:bg-gray-200 focus:outline-none focus:shadow-outline-blue active:bg-gray-800">
           Scrool down
         </button>
-      </div>
+      </m.div>
 
       <style jsx>{`
         video {
@@ -33,7 +33,7 @@ const LandingPageWithVideo = () => {
           min-width: 100%;
         }
       `}</style>
-    </div>
+    </m.div>
   );
 };
 
