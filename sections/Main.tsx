66c4073fd6ef8518266b9be66b6f8065c3147b65
highpriesst <ertansofia@gmail.com>
2022-12-26 01:59:46 +0200

    small tweaks

--- sections/Main.tsx
+++ sections/Main.tsx
@@ -2,9 +2,20 @@ import LandingPageWithVideo from "../components/LandingPageVideo";
 import AboutMe from "./AboutMe";
 import ContactMe from "./ContactMe";
 import Works from "./Works";
+// body {
+//   h-screen overflow-hidden;
+// }
+
+// .parent {
+//   overflow-scroll h-screen scroll-snap-points-y-repeat-h-screen scroll-snap-type-y-mandatory;
+// }
+
+// section {
+//   h-screen scroll-snap-align-start relative;
+// }
 function Landing() {
   return (
-    <div className="bg-white dark:bg-gray-900 font-mono">
+    <div className="bg-white dark:bg-gray-900 font-mono snap-y snap-mandatory h-screen overflow-scroll [&>*]:snap-start">
       <LandingPageWithVideo />
       <AboutMe />
       <Works />
--- sections/Navbar_v2.tsx
+++ sections/Navbar_v2.tsx
@@ -4,7 +4,7 @@ import ThemeToggler from "../components/ThemeToggler";
 function Navbar_v2() {
   return (
     <nav className="bg-transparent fixed top-0 left-0 right-0 z-10 flex items-center justify-between px-4 py-3">
-      <a href="#" className="text-black font-bold text-xl dark:text-white">
+      <a href="#" className="text-gray-200 font-bold text-xl dark:text-white">
         Ertan
       </a>
       <div className="flex items-center space-x-4">
