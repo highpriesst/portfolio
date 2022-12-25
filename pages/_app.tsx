fccabf87ac959504e4d62d0c2b3852f4233f926d
highpriesst <ertansofia@gmail.com>
2022-12-25 20:05:08 +0200

    setting up framer motino

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,25 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import Footer from "../sections/Footer";
 import Navbar_v2 from "../sections/Navbar_v2";
+// import { AnimatePresence } from "framer-motion";
+import { motion as m } from "framer-motion";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
       <Navbar_v2 />
-      <Component {...pageProps} />
+      <m.div
+        animate="animate"
+        variants={{
+          initial: {
+            opacity: 0,
+          },
+          animate: {
+            opacity: 1,
+          },
+        }}>
+        <Component {...pageProps} />
+      </m.div>
       <Footer />
     </ThemeProvider>
   );
