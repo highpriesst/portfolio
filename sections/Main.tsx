f6a8ff9d4e99d58717e65e76e4d2a3f6e310cc46
highpriesst <ertansofia@gmail.com>
2022-12-27 20:44:12 +0200

    overflow issue down and footer

--- sections/Main.tsx
+++ sections/Main.tsx
@@ -2,20 +2,10 @@ import LandingPageWithVideo from "../components/LandingPageVideo";
 import AboutMe from "./AboutMe";
 import ContactMe from "./ContactMe";
 import Works from "./Works";
-// body {
-//   h-screen overflow-hidden;
-// }
 
-// .parent {
-//   overflow-scroll h-screen scroll-snap-points-y-repeat-h-screen scroll-snap-type-y-mandatory;
-// }
-
-// section {
-//   h-screen scroll-snap-align-start relative;
-// }
 function Landing() {
   return (
-    <div className="bg-white dark:bg-gray-900 snap-y snap-mandatory h-screen overflow-scroll  [&>*]:h-screen [&>*]:snap-start ">
+    <div className="bg-white dark:bg-gray-900 snap-y snap-mandatory h-screen overflow-y-auto [&>*]:h-screen [&>*]:snap-start ">
       <LandingPageWithVideo />
       <AboutMe />
       <Works />
