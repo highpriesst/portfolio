bb3bb1337a6da01985576f689eb5d6de51b913b6
highpriesst <ertansofia@gmail.com>
2022-12-27 20:33:24 +0200

    works changes title

--- sections/Works.tsx
+++ sections/Works.tsx
@@ -2,16 +2,8 @@ import Link from "next/link";
 
 function Works() {
   return (
-    <div className=" h-screen flex flex-co ljustify-center items-center indent-8 bg-white dark:bg-gray-900">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
-        veniam minima assumenda exercitationem quia ducimus? Adipisci architecto
-        pariatur, in quam recusandae vero commodi magnam earum aperiam odit
-        assumenda, iste voluptatum?
-      </p>
-      <button className="bg-gray-600 text-white p-2 rounded-lg hover:bg-gray-500">
-        <Link href="/">Home Button</Link>
-      </button>
+    <div className=" h-screen flex flex-col justify-center items-center indent-8 bg-white dark:bg-gray-900">
+      <h1 className="text-3xl">Coming soon...</h1>
     </div>
   );
 }
