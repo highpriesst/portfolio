6cc54f44aa13f3c851041bbca8decf1240c2545b
highpriesst <ertansofia@gmail.com>
2022-12-28 20:02:25 +0200

    new navbar created

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import Image from "next/image";
+import logo_white from "../public/assests/logos/logo_white.png";
 
 function Navbar() {
   return (
-    <div className="flex justify-around w-full p-5 m-5">
-      <span>logo</span>
+    <div className="flex justify-around w-full p-5 m-5 items-center">
+      <Image src={logo_white} className="w-20 h-15 mr-4" alt="logo" />
       <span>themebutton</span>
     </div>
   );
