3bfc30e66c07c34c0c9577281d4f20d6ec778a6b
highpriesst <ertansofia@gmail.com>
2022-12-29 14:31:17 +0200

    initial changes for the new features

---
+++ pages/allWorks.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AllWorks() {
+  return (
+    <div className="h-screen flex justify-center items-center [&>*]:text-white">
+      <h1>coming soon.</h1>
+    </div>
+  );
+}
+
+export default AllWorks;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Links from "../sections/Links";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center h-screen">
+    <div className="flex flex-col items-center h-screen border border-gray-700 ">
       <Navbar />
       <AboutMe />
       <Work />
