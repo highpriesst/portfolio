c0c4607fd88dbd7ff7a9de8b8f3baa643a994d3a
highpriesst <ertansofia@gmail.com>
2022-12-29 17:47:14 +0200

    animation added to outer div

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,19 @@ import Navbar from "../components/Navbar";
 import AboutMe from "../sections/AboutMe";
 import Work from "../sections/Work";
 import Links from "../sections/Links";
+import { motion as m } from "framer-motion";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center h-screen border border-gray-700 ">
+    <m.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ ease: "easeOut", duration: 1.2 }}
+      className="flex flex-col items-center h-screen border border-gray-700 ">
       <Navbar />
       <AboutMe />
       <Work />
       <Links />
-    </div>
+    </m.div>
   );
 }
