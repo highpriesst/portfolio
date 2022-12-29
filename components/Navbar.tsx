1e6501ce7299ea170b8ef2f51740024659b680de
highpriesst <ertansofia@gmail.com>
2022-12-29 14:30:58 +0200

    fixing style issues with mobile view

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,32 @@
 import Image from "next/image";
 import logo_white from "../public/assests/logos/logo_white.png";
+import logo_dark from "../public/assests/logos/logo_dark.png";
+
+import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
+import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 
 function Navbar() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
   return (
     <div className="flex justify-around w-full p-5 m-5 items-center">
-      <Image src={logo_white} className="w-20 h-15 mr-4" alt="logo" />
-      <span>themebutton</span>
+      {theme === "light" ? (
+        <Image src={logo_dark} className="w-20 h-15 mr-4" alt="logo" />
+      ) : (
+        <Image src={logo_white} className="w-20 h-15 mr-4" alt="logo" />
+      )}
+      <button
+        className="w-8 h-8 rounded-lg flex items-center justify-center transition-all duration-300 focus:outline-none"
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        aria-label="Toggle Dark Mode">
+        {theme === "light" ? (
+          <MoonIcon className="text-gray-800 w-5 h-5">Dark</MoonIcon>
+        ) : (
+          <SunIcon className="text-white w-5 h-5 ">Light</SunIcon>
+        )}
+      </button>
     </div>
   );
 }
--- components/SingleLink.tsx
+++ components/SingleLink.tsx
@@ -1,15 +1,22 @@
 import { InferProps, string } from "prop-types";
 import PropTypes from "prop-types";
 
+import { ArrowUpRightIcon } from "@heroicons/react/24/outline";
+
 function SingleLink({
   name,
   link,
   nickname,
 }: InferProps<typeof SingleLink.propTypes>) {
   return (
-    <div className="p-2 flex flex-col [&>*]:mb-1">
-      <span className="text-gray-500">{name}</span>
-      <a href={link}>@{nickname}</a>
+    <div className="flex flex-col [&>*]:mb-2">
+      <span className="text-gray-800 dark:text-gray-500">{name}</span>
+      <div className="flex items-center gap-1">
+        <a className="font-medium dark:font-normal" href={link}>
+          @{nickname}
+        </a>
+        <ArrowUpRightIcon className="h-4 w-4" />
+      </div>
     </div>
   );
 }
