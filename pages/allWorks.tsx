a74665f45b44ade1ef1a8da4ddb8f27206c778f2
highpriesst <ertansofia@gmail.com>
2022-12-31 20:55:22 +0200

    trying to style new component

--- pages/allWorks.tsx
+++ pages/allWorks.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import GreetingText from "../components/GreetingText";
 
 function AllWorks() {
   return (
-    <div className="h-screen flex justify-center items-center [&>*]:text-white">
-      <h1>coming soon.</h1>
+    <div className="h-screen flex justify-center items-center bg-gray-600">
+      <div className="bg-gray-600 flex text-center">
+        <GreetingText />
+      </div>
     </div>
   );
 }
--- sections/Links.tsx
+++ sections/Links.tsx
@@ -3,7 +3,7 @@ import SingleLink from "../components/SingleLink";
 function Links() {
   return (
     <div className="text-black flex flex-col p-5 w-full dark:text-white">
-      <h1 className="mb-3">Links</h1>
+      <h1 className="mb-3 text-[#e4bb68]">Links</h1>
       <SingleLink
         name="Github"
         link={"https://github.com/highpriesst"}
