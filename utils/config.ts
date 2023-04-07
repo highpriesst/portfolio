6f7c2eef9f48c55ed22f17cfa5c4ac54955ab9df
highpriesst <ertansofia@gmail.com>
2023-04-07 18:39:10 +0300

    Updated skills obj and new project obj added.

--- utils/config.ts
+++ utils/config.ts
@@ -1,15 +1,22 @@
 export const skills = [
-  "JavaScript (ES6+)",
+  "JavaScript",
   "TypeScript",
   "React",
   "TailwindCSS",
-  "Node.js",
+  "Node.js/Express",
   "Next.js",
 ];
 
-export const works = {
+export const connectAppProject = {
   year: "2023",
   title: "Connect App",
   desc: "Simple real-time chat application for people who wanna stay anonymous.",
   tech: ["Nextjs", "SocketIO", "TypeScript", "TailwindCSS"],
+};
+
+export const todoProject = {
+  title: "Personal Todo Project",
+  description:
+    "Personal Todo project done with Typescript, React, Express and Mongodb.",
+  link: "https://www.github.com/highpriesst/simple-todo",
 };
