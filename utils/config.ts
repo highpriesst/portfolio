7b5450974fd36636730603af6ffaa80e667fbfbb
highpriesst <ertansofia@gmail.com>
2023-04-22 00:29:21 +0300

    Resusable config works added to list.

--- utils/config.ts
+++ utils/config.ts
@@ -17,6 +17,27 @@ export const connectAppProject = {
 export const todoProject = {
   title: "Personal Todo Project",
   description:
-    "Personal Todo project done with Typescript, React, Express and Mongodb.",
+    "Personal Todo project done with Typescript, React, Express and Mongodb. TailwindCss",
   link: "https://www.github.com/highpriesst/personal-todo",
+};
+
+export const connectApp = {
+  title: "Connect App",
+  description:
+    "Chat application built with Socket.io, Express, Typescript, Next.js. TailwindCss",
+  link: "https://github.com/highpriesst/connect-app",
+};
+
+export const tracktailor = {
+  title: "Track Tailor",
+  description:
+    "(NOT Finished) Small spotify clone to add songs to playlist or create new playlist. Next.js, Typescript, NextAuth, TailwindCss and Spotify's Api.",
+  link: "https://github.com/highpriesst/tracktailor",
+};
+
+export const weatherApp = {
+  title: "Weather App",
+  description:
+    "One of my old classic weather app, done with just Axios, Sass, React.",
+  link: "https://github.com/highpriesst/weather-app",
 };
