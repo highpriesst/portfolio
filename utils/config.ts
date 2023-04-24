748afce7be2f8b51c36fb17d0fbd43324b10cbb9
highpriesst <ertansofia@gmail.com>
2023-04-24 18:30:00 +0300

    Shorter descp.

--- utils/config.ts
+++ utils/config.ts
@@ -7,37 +7,30 @@ export const skills = [
   "Next.js",
 ];
 
-export const connectAppProject = {
-  year: "2023",
-  title: "Connect App",
-  desc: "Simple real-time chat application for people who wanna stay anonymous.",
-  tech: ["Nextjs", "SocketIO", "TypeScript", "TailwindCSS"],
-};
-
 export const todoProject = {
   title: "Personal Todo Project",
   description:
-    "Stay organized with our powerful personal todo project. Built with React, Typescript, Express, and MongoDB, and styled with TailwindCss, managing your daily tasks has never been easier.",
+    "Keep your life organized with our personal todo app. Developed with React, Typescript, Express, MongoDB, and TailwindCss.",
   link: "https://www.github.com/highpriesst/personal-todo",
 };
 
 export const connectApp = {
   title: "Connect App",
   description:
-    "Experience seamless communication with our cutting-edge chat application. It's built with Next.js, Typescript, Express, Socket.io, and TailwindCss to ensure fast, secure, and user-friendly messaging",
+    "Communicate securely and efficiently with our chat app built with Next.js, Typescript, Express, Socket.io, and TailwindCss.",
   link: "https://github.com/highpriesst/connect-app",
 };
 
 export const tracktailor = {
   title: "Track Tailor",
   description:
-    "(NOT Finished) Introducing an innovative app that uses Next.js, Typescript, NextAuth, TailwindCss, and Spotify's API to effortlessly manage and customize Spotify playlists, with easy song addition and removal capabilities.",
+    "(NOT Finished) Easily manage and customize Spotify playlists with Next.js, Typescript, NextAuth, TailwindCss, and Spotify's API.",
   link: "https://github.com/highpriesst/tracktailor",
 };
 
 export const weatherApp = {
   title: "Weather App",
   description:
-    "Never get caught in the rain again with our advanced weather app. Powered by Vite, React, TailwindCss, and Axios, stay up-to-date with real-time weather updates and forecasts with ease.",
+    "Stay ahead of the weather with our app powered by Vite, React, TailwindCss, and Axios. Get real-time updates and forecasts with ease.",
   link: "https://github.com/highpriesst/weather-app",
 };
