12d71ab387c701decc971157fe68492ea6363df1
highpriesst <ertansofia@gmail.com>
2023-06-20 15:47:12 +0300

    Minor style change.

--- sections/Work.tsx
+++ sections/Work.tsx
@@ -10,35 +10,33 @@ import {
 
 function Work() {
   return (
-    <div className="w-full flex h-auto flex-col p-5 md:pl-10 [&>*]:mb-3 lg:w-1/2 lg:p-8">
-      <div>
-        <h1 className="font-bold">
-          <Link href="/allWorks">
-            <h1>Work</h1>
-          </Link>
-        </h1>
+    <div className="w-full flex h-auto flex-col p-5 md:pl-10 lg:w-1/2 lg:p-8">
+      <div className="text-center mb-2">
+        <Link href="/allWorks">Works</Link>
       </div>
-      <div className="grid grid-cols-2 gap-10">
-        <ProjectCard
-          title={todoProject.title}
-          description={todoProject.description}
-          link={todoProject.link}
-        />
-        <ProjectCard
-          title={connectApp.title}
-          description={connectApp.description}
-          link={connectApp.link}
-        />
-        <ProjectCard
-          title={tracktailor.title}
-          description={tracktailor.description}
-          link={tracktailor.link}
-        />
-        <ProjectCard
-          title={weatherApp.title}
-          description={weatherApp.description}
-          link={weatherApp.link}
-        />
+      <div className="flex justify-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 justify-center">
+          <ProjectCard
+            title={todoProject.title}
+            description={todoProject.description}
+            link={todoProject.link}
+          />
+          <ProjectCard
+            title={connectApp.title}
+            description={connectApp.description}
+            link={connectApp.link}
+          />
+          <ProjectCard
+            title={tracktailor.title}
+            description={tracktailor.description}
+            link={tracktailor.link}
+          />
+          <ProjectCard
+            title={weatherApp.title}
+            description={weatherApp.description}
+            link={weatherApp.link}
+          />
+        </div>
       </div>
     </div>
   );
