6fe913928e1a00d7a2096318a3d853dd2d16b8b6
highpriesst <ertansofia@gmail.com>
2023-06-20 16:48:58 +0300

    Removed navbar, fixed theme issue with SingleLink component.

--- components/SingleLink.tsx
+++ components/SingleLink.tsx
@@ -1,22 +1,30 @@
 import { InferProps } from "prop-types";
 import PropTypes from "prop-types";
-
+import { useTheme } from "next-themes";
 import { ArrowUpRightIcon } from "@heroicons/react/24/outline";
+
+import Link from "next/link";
 
 function SingleLink({
   name,
   link,
   nickname,
 }: InferProps<typeof SingleLink.propTypes>) {
+  const { resolvedTheme } = useTheme();
+
   return (
     <div className="flex flex-row [&>*]:mb-2 gap-3">
       <span className="text-[#4c566a]">{name}</span>
       <div className="flex items-center gap-1 group transition duration-300">
-        <a className="font-medium dark:font-normal text-[#8fbcbb]" href={link}>
+        <Link
+          className="font-medium dark:font-normal text-[#8fbcbb]"
+          href={link}>
           @{nickname}
           <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-gray-300"></span>
-        </a>
-        <ArrowUpRightIcon className="h-3 w-3 ml-1 dark:text-white" />
+        </Link>
+        <p className="text-white dark:text-white">
+          <ArrowUpRightIcon className={`h-3 w-3 ml-1`} />
+        </p>
       </div>
     </div>
   );
